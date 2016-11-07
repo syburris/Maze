855c8b2829434d952270cbccb4648bedbb16a984
Steven Burris <stevenburris.jr@gmail.com>
2016-11-07 10:36:13 -0500

    Added room class/finished grid;

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,5 +1,10 @@
 package com.theironyard;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.logging.Filter;
+import java.util.stream.Collectors;
+
 public class Main {
 
     static final int SIZE = 10;
@@ -12,6 +17,37 @@ public class Main {
             }
         }
         return rooms;
+    }
+
+    static ArrayList<Room> possibleNeighbors(Room[][] rooms, int row, int col) {
+        ArrayList<Room> neighbors = new ArrayList<>();
+        if (row < SIZE - 1) {
+            neighbors.add(rooms[row + 1][col]);
+        }
+        if (row > 0) {
+            neighbors.add(rooms[row - 1][col]);
+        }
+        if (col < SIZE - 1) {
+            neighbors.add(rooms[row][col + 1]);
+        }
+        if (col > 0) {
+            neighbors.add(rooms[row][col - 1]);
+        }
+        neighbors = neighbors.stream()
+                .filter(room -> !room.wasVisited)
+                .collect(Collectors.toCollection((ArrayList::new)));
+        return neighbors;
+    }
+
+    static Room randomNeighbor(Room[][] rooms, int row, int col) {
+        ArrayList<Room> neighbors = possibleNeighbors(rooms, row, col);
+        if (neighbors.size() == 0) {
+            return null;
+        }
+
+        Random r = new Random();
+        int index = r.nextInt(neighbors.size());
+        return neighbors.get(index);
     }
 
     public static void main(String[] args) {
